e6ad36a9abc66deca91cbe2f0daa4fa4ffa21a1f
Alpine <95679722+Alpine-Oracle@users.noreply.github.com>
2022-08-31 20:56:16 -0300

    Updated header to discoin from adao

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className='font-primary font-bold m-4 text-5xl'>Faucet for Cardano Native Assets
-            <p className='m-4 text-sm'>As a token of appreciation all early discord supporters of ADAO
-            are eligible to redeem the $ADAO governance token!     
+        <header className='self-center font-primary border-b-2 font-bold m-8 text-5xl'>
+        <img width={400} src="https://cdn.discordapp.com/attachments/939703759951507527/1014238386824941628/discoinclaim.png" />
+            <p className='m-2 text-sm'>Discoin is imminent
             </p>
         </header>
     )
