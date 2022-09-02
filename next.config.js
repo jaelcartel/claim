42c5d721353ef61622f0237a9aba47fb68233102
dgarey <72640038+dgarey@users.noreply.github.com>
2022-09-02 11:41:55 -0500

    Revert "5 lucid webpack experimental feature compatibility"

--- next.config.js
+++ next.config.js
@@ -2,15 +2,10 @@ module.exports = {
     webpack: (config) => {
   
       // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-      config.experiments = { 
-        asyncWebAssembly: true,
-        topLevelAwait: true,
-        layers: true,
-	    }
+      config.experiments = { asyncWebAssembly: true, layers: true }
   
       return config
     },
-    output: 'standalone',
     env: {
         COSMOS_KEY: '-',
         COSMOS_ENDPOINT: '-',
