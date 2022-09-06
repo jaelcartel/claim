13b168c100ea092bf12c0b0e3f617bd80bfd273c
Alpine <95679722+Alpine-Oracle@users.noreply.github.com>
2022-09-05 21:50:05 -0300

    Add Check Address Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,7 @@ export default function Index(props: Props) {
           <link rel="icon" href="/Cartel.jpg" />
         </Head>
         <Header />
-        <main className="flex-grow justify-center items-center p-10">
+        <main className="flex-grow justify-center items-center pt-20">
         {/* <h1>
           Hey, {props.user.username}#{props.user.discriminator}
         </h1> */}
@@ -110,9 +110,13 @@ export default function Index(props: Props) {
           </>
           : 
           <>
-            <h2>{claimStatus === '' ? "Enter your Cardano Wallet Address" : claimStatus}</h2>
-            <div className= 'justify center pt-8 pl-20 pr-20 pb-8 text-black'>
-            <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="addr1..." type="text" name="search"/>
+            <h2>{claimStatus === '' ? "Enter Cardano Wallet Address" : claimStatus}</h2>
+              <div className= 'justify center pt-2 pl-20 pr-20 pb-8 text-black'>
+                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="addr1..." type="text" name="search"/>
+              <div className="flex space-x-2 justify-center pt-4">
+                  <button type="button" className="inline-block px-6 py-2 border-2 border-purple-400 text-white font-bold text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                >Check Address</button>
+              </div>
             </div>
           </>
         }
