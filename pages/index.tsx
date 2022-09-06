57c0922ee9ca44dd668d714b4aa3cf43f686f92b
Alpine <95679722+Alpine-Oracle@users.noreply.github.com>
2022-09-06 00:17:08 -0300

    Change textbox border color

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,7 +112,7 @@ export default function Index(props: Props) {
           <>
             <h2>{claimStatus === '' ? "Enter Cardano Wallet Address" : claimStatus}</h2>
               <div className= 'justify center pt-2 pl-20 pr-20 pb-8 text-black'>
-                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="addr1..." type="text" name="search"/>
+                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-3 shadow-sm focus:outline-none focus:border-pink-200 focus:ring-pink-200 focus:ring-1 sm:text-sm" placeholder="addr1..." type="text" name="search"/>
               <div className="flex space-x-2 justify-center pt-4">
                   <button type="button" className="inline-block px-6 py-2 border-2 border-purple-400 text-white font-bold text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-10 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                 >Check Address</button>
